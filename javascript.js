956d150406a3aeb4144c96c216ef6d6fc47e4f30
gabrcs <68124379+gabrcs@users.noreply.github.com>
2020-11-13 17:51:26 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,39 +1,39 @@
 
 var bloco1,bloco2,bloco3,bloco4,bloco5,bloco6,bloco7,bloco8,bloco9,bloco10,bloco11,bloco12,bloco13,bloco14,bloco15;
-var calledNumbers = new Array();
+var numUsados = new Array();
 
 function geraCartela(){
   var  b1 = Math.floor(Math.random() * 20) + 1;
-  var  b2 = Math.floor(Math.random() * 40) + 21;
-  var  b3 = Math.floor(Math.random() * 60) + 41;
+  var  b2 = Math.floor(Math.random() * (40 - 21)) + 21;
+  var  b3 = Math.floor(Math.random() * (60 - 41)) + 41;
   var  b4 = Math.floor(Math.random() * 20) + 1;
-  var  b5 = Math.floor(Math.random() * 40) + 21;
-  var  b6 = Math.floor(Math.random() * 60) + 41;
+  var  b5 = Math.floor(Math.random() * (40 - 21)) + 21;
+  var  b6 = Math.floor(Math.random() * (60 - 41)) + 41;
   var  b7 = Math.floor(Math.random() * 20) + 1;
-  var  b8 = Math.floor(Math.random() * 40) + 21;
-  var  b9 = Math.floor(Math.random() * 60) + 41;
+  var  b8 = Math.floor(Math.random() * (40 - 21)) + 21;
+  var  b9 = Math.floor(Math.random() * (60 - 41)) + 41;
   var  b10 = Math.floor(Math.random() * 20) + 1;
-  var  b11 = Math.floor(Math.random() * 40) + 21;
-  var  b12 = Math.floor(Math.random() * 60) + 41;
+  var  b11 = Math.floor(Math.random() * (40 - 21)) + 21;
+  var  b12 = Math.floor(Math.random() * (60 - 41)) + 41;
   var  b13 = Math.floor(Math.random() * 20) + 1;
-  var  b14 = Math.floor(Math.random() * 40) + 21;
-  var  b15 = Math.floor(Math.random() * 60) + 41;
+  var  b14 = Math.floor(Math.random() * (40 - 21)) + 21;
+  var  b15 = Math.floor(Math.random() * (60 - 41)) + 41;
 
-  bloco1 = document.getElementById("square0").innerHTML = b1;
-  bloco2 = document.getElementById("square1").innerHTML = b2;
-  bloco3 = document.getElementById("square2").innerHTML = b3;
-  bloco4 = document.getElementById("square3").innerHTML = b4;
-  bloco5 = document.getElementById("square4").innerHTML = b5;
-  bloco6 = document.getElementById("square5").innerHTML = b6;
-  bloco7 = document.getElementById("square6").innerHTML = b7;
-  bloco8 = document.getElementById("square7").innerHTML = b8;
-  bloco9 = document.getElementById("square8").innerHTML = b9
-  bloco10 = document.getElementById("square9").innerHTML = b10;
-  bloco11 = document.getElementById("square10").innerHTML = b11;
-  bloco12 = document.getElementById("square11").innerHTML = b12;
-  bloco13 = document.getElementById("square12").innerHTML = b13;
-  bloco14 = document.getElementById("square13").innerHTML = b14;
-  bloco15 = document.getElementById("square14").innerHTML = b15;
+  bloco1 = document.getElementById("bloco0").innerHTML = b1;
+  bloco2 = document.getElementById("bloco1").innerHTML = b2;
+  bloco3 = document.getElementById("bloco2").innerHTML = b3;
+  bloco4 = document.getElementById("bloco3").innerHTML = b4;
+  bloco5 = document.getElementById("bloco4").innerHTML = b5;
+  bloco6 = document.getElementById("bloco5").innerHTML = b6;
+  bloco7 = document.getElementById("bloco6").innerHTML = b7;
+  bloco8 = document.getElementById("bloco7").innerHTML = b8;
+  bloco9 = document.getElementById("bloco8").innerHTML = b9
+  bloco10 = document.getElementById("bloco9").innerHTML = b10;
+  bloco11 = document.getElementById("bloco10").innerHTML = b11;
+  bloco12 = document.getElementById("bloco11").innerHTML = b12;
+  bloco13 = document.getElementById("bloco12").innerHTML = b13;
+  bloco14 = document.getElementById("bloco13").innerHTML = b14;
+  bloco15 = document.getElementById("bloco14").innerHTML = b15;
   
   var sq1 = b1;
   var sq2 = b2;
@@ -54,7 +54,7 @@ function geraCartela(){
   for (var i = 0; i < 25; i++) {
         if (i == 12)
             continue;
-        var currentSquare = document.getElementById("square" + i);
+        var currentSquare = document.getElementById("bloco" + i);
         currentSquare.style.backgroundColor = "#ffffff";
     }
     return;
@@ -71,39 +71,39 @@ function sorteia(){
        numSorteados.push(numAtual);
           var div = document.getElementById("textSorteio");
           div.innerText = "Números sorteados:" ;
-          calledNumbers.push(numAtual);
-          document.getElementById("numSorteados").innerHTML = calledNumbers;
+          numUsados.push(numAtual);
+          document.getElementById("numSorteados").innerHTML = numUsados;
       }
     }
   }
 } 
 
-function seleciona(square) {
-  var currentSquare = document.getElementById(square);
-  if (currentSquare.style.backgroundColor == "lightblue") 
-      currentSquare.style.backgroundColor = "lightblue";
+function seleciona(bloco) {
+  var blocoAtual = document.getElementById(bloco);
+  if (blocoAtual.style.backgroundColor == "lightblue") 
+      blocoAtual.style.backgroundColor = "lightblue";
   else
-      currentSquare.style.backgroundColor = "lightblue";
+      blocoAtual.style.backgroundColor = "lightblue";
   return;
 }
 
 function verifNum(sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq9,sq10,sq11,sq12,sq13,sq14,sq15){
   
-  sq1 = document.getElementById("square0");
-  sq2 = document.getElementById("square1");
-  sq3 = document.getElementById("square2");
-  sq4 = document.getElementById("square3");
-  sq5 = document.getElementById("square4");
-  sq6 = document.getElementById("square5");
-  sq7 = document.getElementById("square6");
-  sq8 = document.getElementById("square7");
-  sq9 = document.getElementById("square8");
-  sq10 = document.getElementById("square9");
-  sq11 = document.getElementById("square10");
-  sq12 = document.getElementById("square11");
-  sq13 = document.getElementById("square12");
-  sq14 = document.getElementById("square13");
-  sq15 = document.getElementById("square14");
+  sq1 = document.getElementById("bloco0");
+  sq2 = document.getElementById("bloco1");
+  sq3 = document.getElementById("bloco2");
+  sq4 = document.getElementById("bloco3");
+  sq5 = document.getElementById("bloco4");
+  sq6 = document.getElementById("bloco5");
+  sq7 = document.getElementById("bloco6");
+  sq8 = document.getElementById("bloco7");
+  sq9 = document.getElementById("bloco8");
+  sq10 = document.getElementById("bloco9");
+  sq11 = document.getElementById("bloco10");
+  sq12 = document.getElementById("bloco11");
+  sq13 = document.getElementById("bloco12");
+  sq14 = document.getElementById("bloco13");
+  sq15 = document.getElementById("bloco14");
   
   var cout = 0;
 
